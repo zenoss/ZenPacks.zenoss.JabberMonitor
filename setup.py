601732ff0d6b2eeb2aa8a46bbf0ff38451e179ff
bedwards <bedwards@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-27 20:53:07 +0000

    Fixes ZEN-2725. Reviewed by Danny. Add marker egg to detect version.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.JabberMonitor@62062 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.JabberMonitor']
-INSTALL_REQUIRES = []
+INSTALL_REQUIRES = ['Zenoss >= 2.5.70']
 COMPAT_ZENOSS_VERS = '>=2.5.70'
 PREV_ZENPACK_NAME = ''
 # STOP_REPLACEMENTS
